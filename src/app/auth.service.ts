7ff2182531c7a8d21c472a8219b5860b53a57f85
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-05-03 21:17:53 +0200

    feat: call auth action after check auth state

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Store } from '@ngrx/store';
-import { from, map } from 'rxjs';
+import { from, map, tap } from 'rxjs';
+import { AppState } from './store/app.state';
+import { AuthActions } from './store/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth) {}
+  constructor(private auth: AngularFireAuth, private store: Store<AppState>) {}
 
   login(username: string, password: string) {
     return from(this.auth.signInWithEmailAndPassword(username, password));
@@ -19,6 +21,10 @@ export class AuthService {
 
   isAuth() {
     return this.auth.authState.pipe(
+      tap((user) => {
+        if (user) this.store.dispatch(AuthActions.signIn());
+        else this.store.dispatch(AuthActions.signOut());
+      }),
       map((user) => {
         if (user) return true;
         else return false;
