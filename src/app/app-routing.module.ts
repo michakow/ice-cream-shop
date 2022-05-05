c5b6fb6d2d2f20dfd812a2e8fc43b71cd190a861
Michal Kowalczyk <michal.kowalczyk@ppuc.pl>
2022-05-05 15:06:38 +0200

    feat: add role guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ClientComponent } from './client/client.component';
+import { AdminRoleGuard } from './guards/admin-role.guard';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginPanelGuard } from './guards/login-panel.guard';
+import { UserRoleGuard } from './guards/user-role.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginPanelComponent } from './login-panel/login-panel.component';
 
@@ -15,6 +19,42 @@ const routes: Routes = [
     path: 'home',
     component: HomeComponent,
     canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'admin',
+        component: AdminComponent,
+        canActivate: [AdminRoleGuard],
+        children: [
+          {
+            path: 'clients',
+            component: AdminComponent,
+          },
+          {
+            path: 'flavors',
+            component: AdminComponent,
+          },
+          {
+            path: 'orders',
+            component: AdminComponent,
+          },
+        ],
+      },
+      {
+        path: 'client',
+        component: ClientComponent,
+        canActivate: [UserRoleGuard],
+        children: [
+          {
+            path: 'flavors',
+            component: ClientComponent,
+          },
+          {
+            path: 'orders',
+            component: ClientComponent,
+          },
+        ],
+      },
+    ],
   },
   {
     path: '',
---
+++ src/app/guards/admin-role.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { UserService } from '../user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminRoleGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(): Observable<boolean> {
+    return this.userService.getUserRole().pipe(
+      map((role) => {
+        if (role === 'admin') return true;
+        else {
+          this.router.navigate(['home']);
+          return false;
+        }
+      })
+    );
+  }
+}
