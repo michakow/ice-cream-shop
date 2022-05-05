ebf057e1fa48d78a97dc7702d5d4775fc6727f50
Michal Kowalczyk <michal.kowalczyk@ppuc.pl>
2022-05-05 15:08:28 +0200

    feat: add navigation to home page

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRippleModule } from '@angular/material/core';
 
 const MaterialComponents = [
   MatButtonModule,
   MatToolbarModule,
   MatProgressSpinnerModule,
+  MatRippleModule,
 ];
 
 @NgModule({
