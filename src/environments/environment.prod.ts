1c26da386260e55ff94ba065c7912156ad44d72c
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-05-08 21:10:20 +0200

    feat: changes for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  firebaseConfig: {
+    apiKey: 'AIzaSyDR0WDSrWKK-vJBuVGi-AFxUZJEHgS8xHY',
+    authDomain: 'ice-cream-shop-45821.firebaseapp.com',
+    databaseURL:
+      'https://ice-cream-shop-45821-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: 'ice-cream-shop-45821',
+    storageBucket: 'ice-cream-shop-45821.appspot.com',
+    messagingSenderId: '561269820096',
+    appId: '1:561269820096:web:10e51a3921eaa4f3aefb25',
+  },
+  defaultPassword: 'zaq1@WSX',
+  production: true,
 };
