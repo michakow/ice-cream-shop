1d9465a272e544bce9790d95244c133188fe2574
Michal Kowalczyk <michal.kowalczyk@ppuc.pl>
2022-05-11 13:20:25 +0200

    fix: fix add unit form validation

--- src/app/unit-list/unit-list.service.ts
+++ src/app/unit-list/unit-list.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { map } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject, map, tap } from 'rxjs';
 import { FormCreatorService } from '../form-creator.service';
 import { Unit } from '../models/unit.model';
 
@@ -8,13 +9,23 @@ import { Unit } from '../models/unit.model';
   providedIn: 'root',
 })
 export class UnitListService {
+  private unitList: BehaviorSubject<Unit[]> = new BehaviorSubject<Unit[]>([]);
+
   constructor(
     private db: AngularFirestore,
-    private formCreatorService: FormCreatorService
+    private formCreatorService: FormCreatorService,
+    private toast: ToastrService
   ) {}
 
   public addUnit(unitValue: number) {
     const docID = this.generateDocID(28);
+    if (this.checkIfUnitExist(unitValue)) {
+      this.toast.error(
+        'Pojemność o takim rozmiarze istnieje',
+        'Pojemność istnieje'
+      );
+      return;
+    }
     this.db.doc<Unit>(`units/${docID}`).set(
       {
         value: unitValue,
@@ -28,7 +39,10 @@ export class UnitListService {
     return this.db
       .collection<Unit>('units')
       .valueChanges()
-      .pipe(map((units) => units.sort((a, b) => a.value - b.value)));
+      .pipe(
+        map((units) => units.sort((a, b) => a.value - b.value)),
+        tap((units) => this.unitList.next(units))
+      );
   }
 
   public generateDocID(length: number) {
@@ -39,6 +53,16 @@ export class UnitListService {
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
+    return result;
+  }
+
+  public checkIfUnitExist(value: number) {
+    let result = false;
+    this.unitList.getValue().forEach((unit) => {
+      if (unit.value === value) {
+        result = true;
+      }
+    });
     return result;
   }
 
